de006c9c2ef796d09b236755aa1139c9bd1bcc09
flatlineratheist <131427314+flatlineratheist@users.noreply.github.com>
2025-01-24 12:01:18 +0530

    Update deployer.groovy

--- vars/deployer.groovy
+++ vars/deployer.groovy
@@ -48,7 +48,7 @@ spec:
                         # Optionally, set KUBECONFIG environment variable to use this kubeconfig
                         export KUBECONFIG=/root/.kube/config
                         kubectl config get-contexts
-                        # kubectl get nodes
+                        kubectl get nodes
                         kubectl get namespaces
                     """
                 }
